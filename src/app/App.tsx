daad47dba94acead19f50dc45e34e5839cb4e974
VlaRu <dn210690nvs@gmail.com>
2025-02-03 00:39:10 +0300

    feat: perform cards result section

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <p className="read-the-docs">Searching your pokemon!</p>
         <Search />
       </>
     );
