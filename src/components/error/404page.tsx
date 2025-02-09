1354cd8a853b9a04f8f3db0661a91d7f3a1191a6
VlaRu <dn210690nvs@gmail.com>
2025-02-09 15:24:40 +0300

    feat: add 404 page

---
+++ src/components/error/404page.tsx
@@ -0,0 +1,12 @@
+export default function ErrorPage() {
+  return (
+    <div>
+      <div>
+        <p>404 Route</p>
+        <p>Not Found</p>
+        <p>The requested URL was not found on this server.</p>
+        {/* <Link to="/">to main page</Link> */}
+      </div>
+    </div>
+  );
+}
