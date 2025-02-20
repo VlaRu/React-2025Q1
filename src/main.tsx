b7052f8d977072389228848c931403564f6b7925
VlaRu <dn210690nvs@gmail.com>
2025-02-20 13:57:22 +0300

    feat: implement redux store

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import App from './app/App.tsx';
 import ErrorBoundary from './components/error/ErrorBoundary.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
